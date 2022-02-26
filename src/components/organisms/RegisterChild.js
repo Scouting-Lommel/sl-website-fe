08007dc08c21274079f402cb91b35dbbb22e7303
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-02-26 11:17:55 +0100

    big improvements

--- src/components/organisms/RegisterChild.js
+++ src/components/organisms/RegisterChild.js
@@ -1,18 +1,21 @@
-export default function RegisterChild(){
-    return (
+import { useState } from 'react';
+
+export default function RegisterChild({id}){
+  const [isVisible, setVisible] = useState(false);
+  return (
         <div className="bg-white rounded basis-1/2 px-8 =mb-4 flex flex-col justify-center gap-4 max-w-lg">
         <div className="flex flex-row justify-center gap-4">
             <div className="flex flex-col justify-center">
               <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="firstName">
                   Voornaam
               </label>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="firstName" type="text" placeholder="Voornaam"/>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id={"firstName"+id} type="text" placeholder="Voornaam"/>
             </div>
             <div className="flex flex-col justify-center">
               <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="lastName">
                   Achternaam
               </label>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="lastName" type="text" placeholder="Achternaam"/>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id={"lastName"+id} type="text" placeholder="Achternaam"/>
             </div>
         </div>
         <div className="flex flex-row justify-center gap-4">
@@ -20,18 +23,36 @@ export default function RegisterChild(){
             <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="birthday">
                 Geboortedatum
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="birthday" type="date"/>
+            <input onChange={() => {guessTak(id)}} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id={"birthday"+id} type="date"/>
           </div>
           <div className="flex flex-col justify-end gap-4">
             <div className="flex flex-row justify-center gap-1">
-              <input id="akabe" name="akabe" type="checkbox" />
+              <input id={"akabe"+id} name="akabe" type="checkbox" onChange={() => {guessTak(id)}}/>
               <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="akabe">
                   Akabe
               </label>
             </div>
           </div>
-          <input className=" appearance-none text-center rounded max-w-md py-2 px-3 text-grey-darker" type="text" id="tak" value="None" readOnly/>
+          <input className=" appearance-none text-center rounded max-w-md py-2 px-3 text-grey-darker" type="text" id={"tak"+id} value="None" readOnly/>
         </div>
+        <hr className="shadow fill-black"></hr>
+        {isVisible && 
+          <RegisterChild id={id+1} />
+        }
+        {!isVisible && <div className="flex flex-row justify-center">
+          <button type="button" className="border shadow rounded max-w-fit p-2" onClick={() => {setVisible(true)}}>Voeg kind toe</button>
+        </div>}
     </div>
     )
+}
+
+function guessTak(id){
+  let calender = document.getElementById("birthday"+id)
+  let tak = document.getElementById("tak"+id)
+  let akabe = document.getElementById("akabe"+id)
+  if(akabe.checked){
+    tak.value="Akabe"
+    return;
+  }
+ tak.value = "TODO"
 }
--- src/pages/inschrijven.js
+++ src/pages/inschrijven.js
@@ -8,10 +8,7 @@ export default function inschrijven() {
     <div className="flex flex-row justify-center py-32 ">
     <div className="bg-white shadow-md rounded basis-1/2 px-8 pt-6 pb-8 mb-4 flex flex-col justify-center gap-4 max-w-lg">
       <div id="childrenHeader">
-       <RegisterChild />
-      </div>
-      <div className="flex flex-row justify-center">
-        <button type="button" className="border shadow rounded max-w-fit p-2" onClick={() => {addChild()}}>Voeg kind toe</button>
+       <RegisterChild id={0}/>
       </div>
       <RegisterInfo />
       <div className="flex flex-row justify-center">
@@ -24,9 +21,61 @@ export default function inschrijven() {
 }
 
 function register(){
-  
-}
-
-function addChild(){
-
+  const streetName = document.getElementById("street").value
+  const houseNumber = document.getElementById("number").value
+  const bus = document.getElementById("bus").value
+  const postCode = document.getElementById("postcode").value
+  const city = document.getElementById("city").value
+  const phone = document.getElementById("tel").value
+  const gsm = document.getElementById("gsm").value
+  const email = document.getElementById("email").value
+  const privacy = document.getElementById("privacy").checked
+  if(!privacy){
+    console.log("not checked privacy aggreement")
+    return
+  }
+  // todo add requirement specifics for some
+  if(!streetName){
+    return
+  }else if(!houseNumber){
+    return
+  }else if(!postCode){
+    return
+  }else if(!city){
+    return
+  }else if(!phone){
+    return
+  }else if(!email){
+    return
+  }
+  // get every child info
+  let firstnames = []
+  let lastnames = []
+  let birthdays = []
+  let akabeLst = []
+  for (let i = 0; i < 200; i++) {
+    const fName = document.getElementById("firstName"+i)
+    if(!fName) break
+    const firstName = fName.value;
+    const lastName =  document.getElementById("lastName"+i).value
+    const birthday = document.getElementById("birthday"+i).value
+    const akabe = document.getElementById("akabe"+i).checked
+    // TODO: add required info for each
+    if(!firstName){
+      return
+    }else if(!lastName){
+      return
+    }else if(!birthday){
+      return
+    }
+    firstnames.push(firstName)
+    lastnames.push(lastName)
+    birthdays.push(birthday)
+    akabeLst.push(akabe)
+  }
+  //TODO: send to server
+  console.log(firstnames)
+  console.log(lastnames)
+  console.log(birthdays)
+  console.log(akabeLst)
 }
