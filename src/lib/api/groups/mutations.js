0575abd990510f1e109a78a63b76cf13d20c15d1
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-03-06 21:29:02 +0100

    Basic mutations for group

--- src/lib/api/groups/mutations.js
+++ src/lib/api/groups/mutations.js
@@ -1,1 +1,78 @@
 import {gql} from '@apollo/client';
+
+const createActivity = () => {
+    return gql`
+        mutation (
+                $title: String
+                $description: String
+                $startTime: DateTime
+                $endTime: DateTime
+                $takID: ID!
+            ) {
+            createActivity(
+                data: {
+                startTime: $startTime
+                endTime: $endTime
+                description: $description
+                Title: $title
+                groups: $takID
+                }
+            ) {
+                data {
+                id
+                }
+            }
+        }`
+}
+
+const editActivity = () => {
+    return gql`
+    mutation (
+      $title: String
+      $description: String
+      $startTime: DateTime
+      $endTime: DateTime
+      $actID: ID!
+    ) {
+      updateActivity(
+        id: $actID
+        data: {
+          startTime: $startTime
+          endTime: $endTime
+          description: $description
+          Title: $title
+        }
+      ) {
+        data {
+          id
+        }
+      }
+    }
+  `
+}
+
+const createFile = () => {
+    return gql`
+    mutation ($file: Upload!, $name: String) {
+      upload(info: { name: $name }, file: $file) {
+        data {
+          id
+        }
+      }
+    }
+  `;
+}
+
+const linkFileToGroup = () => {
+    return gql`
+    mutation ($groupID: ID!, $fileIDs: [ID]) {
+      updateGroup(id: $groupID, data: { Files: $fileIDs }) {
+        data {
+          id
+        }
+      }
+    }
+  `
+}
+
+export {createActivity, editActivity, createFile, linkFileToGroup}
