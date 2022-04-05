e745036734a4dfd7595f4c8b30827db88aa704c7
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-04-05 21:23:43 +0200

    Button style

--- src/components/atoms/ComponentLink.js
+++ src/components/atoms/ComponentLink.js
@@ -2,11 +2,12 @@ import Link from 'next/link'
 
 export default function ComponentLink({info}){
     if(!info || !info.Page) return(<></>)
+    const style = info.IsButton ? " bg-blue-600 hover:bg-white text-white hover:text-blue-600 font-bold rounded border-2 border-blue-600 py-2 px-4" : "";
     return (
-        <>
-        <Link href={info.Page} className="text-center">
-            <a className="px-4 text-center">{info.Label}</a>
-        </Link>
+        <>        
+            <Link href={info.Page}>
+                <a className={"text-center" + style}>{info.Label}</a>
+            </Link>    
         </>
     )
 }
