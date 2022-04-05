e0fbdcff551e38f6357ad614908c8809517385cc
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-04-05 21:56:20 +0200

    Just a little out of mind styling out of boredom

--- src/components/organisms/CallToAction.js
+++ src/components/organisms/CallToAction.js
@@ -4,13 +4,15 @@ const CallToAction = ({info}) => {
     console.log(info)
     return(
         <>
-        <div className="flex flex-row justify-between bg-blue-400 py-4">
-            <div>
-                <h1 className="text-3xl font-bold pl-2">{info.Title}</h1>
-                <p className="pl-2">{info.Content}</p>
-            </div>
-            <div className="flex flex-col justify-center">
-                <ComponentLink info={info.Button}/>
+        <div className="px-11 py-4">
+            <div className="flex flex-row justify-between bg-white border-2 border-blue-600 rounded-2xl py-4">
+                <div>
+                    <h1 className="text-3xl font-bold pl-2">{info.Title}</h1>
+                    <p className="pl-2">{info.Content}</p>
+                </div>
+                <div className="flex flex-col justify-center px-2">
+                    <ComponentLink info={info.Button}/>
+                </div>
             </div>
         </div>
         </>
