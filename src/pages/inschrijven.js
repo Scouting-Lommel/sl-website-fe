42e81a6d64a2aeb0d4afa266101156ee4573bdc1
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-07-02 13:33:21 +0200

    fix formating on inschrijf page

--- src/pages/inschrijven.js
+++ src/pages/inschrijven.js
@@ -22,7 +22,7 @@ export default function inschrijven({fin, general}) {
         <title>{fin.Title}</title>
         {fin.NoIndex && <meta name="googlebot" content="noindex"/>}
       </Head>
-    <div className="flex flex-row justify-center py-32 ">
+    <div className="flex flex-row justify-center py-14 ">
     {!isPaying &&
     <div className="bg-white shadow-md rounded basis-1/2 px-8 pt-6 pb-8 mb-4 flex flex-col justify-center gap-4 max-w-lg">
       <div id="childrenHeader">
@@ -57,7 +57,7 @@ export default function inschrijven({fin, general}) {
       <div>Gelieve {getFinalLeaders.length*fin.LeaderPrice + getFinalChildren.length*fin.ChildPrice} euro over te schrijven op het volgende rekeningnummer:</div>
       <div>{fin.AcountNr}</div>
       <div>Met vermelding:</div>
-      <div>Inschrijving {getFinalChildren.join() +","+ getFinalLeaders.join()}</div>
+      <div>Inschrijving {getFinalChildren.concat(getFinalLeaders).join(", ")}</div>
     </div>}
     </div>
     </Layout>
