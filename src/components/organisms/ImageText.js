a59b80106aa40838e5bf65dabe0ea74595eb3464
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-07-02 14:43:13 +0200

    more small bugs fixed

--- src/components/organisms/ImageText.js
+++ src/components/organisms/ImageText.js
@@ -2,16 +2,19 @@ import ComponentImage from "../atoms/ComponentImage"
 import ComponentLink from "../atoms/ComponentLink"
 
 const ImageText = ({info}) => {
-    const leftAlligned = info.ImageLeftAligned ? "flex flex-row-reversed" : "flex flex-row";
+    console.log(info)
+    const leftAlligned = info.ImageLeftAligned ? "flex-row-reverse" : "flex-row";
     return(
         <>
-        <div className={leftAlligned}>
+        <div className={"flex " + leftAlligned}>
             <div className="max-w-xl">
                 <h1 className="text-5xl font-bold py-2">{info.Title}</h1>
                 <pre className="whitespace-pre-wrap">{info.Content}</pre>
             </div>
-            <div className="flex justify-center flex-auto">
-                <ComponentImage args={info.Image} />
+            <div className="flex flex-col justify-center flex-auto">
+                <div className="flex justify-center">
+                    <ComponentImage args={info.Image} />
+                </div>
             </div>
         </div>
         </>
