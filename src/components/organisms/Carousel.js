a00e82c621407953b40e56dee3101cf02c1a95aa
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-07-09 16:23:16 +0200

    created page with all leaders on display

--- src/components/organisms/Carousel.js
+++ src/components/organisms/Carousel.js
@@ -9,18 +9,15 @@ const ItemCarousel = ({info, leaders}) => {
   const topRow = info.Items.slice(0, topLength)
   const bottomRow = info.Items.slice(topLength, info.Items.length)
   return(
-    <>
-    <h2 className="py-3 flex justify-around text-4xl font-bold">{info.Title}</h2>
+    <div className="rounded-md shadow-sm border-black border-2">
+    <h2 className="py-3 flex justify-around text-4xl font-bold border-b-2 border-black">{info.Title}</h2>
       {info.IsLeaderShowcase && !info.IsGroupsShowcase && leaders != undefined &&
-        <>
-          <div className="flex justify-around pb-4">
+          <div className="flex justify-around py-4">
             {leaders.data.map((item, index) => {
               return <CarouselItem info={item.attributes} key={"carItem" + index}/>
             })}
-          </div>
-        </>}
+          </div>}
       {!info.IsLeaderShowcase && info.IsGroupsShowcase && 
-      <>
       <div className="grid grid-rows-2 gap-2 bg-green-700 py-2">
         <div className="flex flex-row justify-center gap-x-2">
             {topRow.map((item, index) => {
@@ -37,9 +34,8 @@ const ItemCarousel = ({info, leaders}) => {
         </div>
         </div>
       </div>
-      </>
       }
-    </>
+    </div>
   )
 };
 
