d79b7a89f1dd105c5d94a30806d5cd2a19d85b6a
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-08-11 09:16:18 +0200

    Add link to logo

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -13,9 +13,12 @@ const Header = ({ info }) => {
   return (
     <>
       <div className="flex flex-row pr-5 py-2 border-b-2 border-black pl-5">
-        <div className=" h-14 w-1/12 relative">
-          <Image src={info.Logo.data.attributes.url} alt="image" />
-        </div>
+        <Link href="/">
+          <a className=" h-14 w-1/12 relative">
+            <Image src={info.Logo.data.attributes.url} alt="image" />
+          </a>
+        </Link>
+
         <div className="grow"></div>
         {info.NavigationItems.map((item, i) => {
           return <Navigation info={item} key={"Headernav" + i} />;
