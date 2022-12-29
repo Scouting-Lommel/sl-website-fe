f6129d63267a95d7bc84acbc94e41846e86ec38c
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-12-29 19:00:22 +0100

    Fix i18n import

--- i18n.js
+++ i18n.js
@@ -3,7 +3,7 @@ const { locales, defaultLocale } = require("./src/locales");
 // TODO: Clean up workaround when an official fix is available
 const workaround = require("next-translate/lib/cjs/plugin/utils.js");
 workaround.defaultLoader =
-  "(l, n) => import(`lang/${l}/${n}.json`).then(m => m.default)";
+  "(l, n) => import(`/src/locales/${l}/${n}.json`).then(m => m.default)";
 
 module.exports = {
   locales: locales,
