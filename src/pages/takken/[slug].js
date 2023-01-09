34e2d3b248871276c326e91d55f95c5b12c2c233
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-01-09 11:01:36 +0100

    Add group page

---
+++ src/pages/takken/[slug].js
@@ -0,0 +1,55 @@
+import client from "@/lib/api/apollo/client";
+import { getGeneralData } from "@/lib/api/general";
+import { getGroupPage } from "@/lib/api/groups";
+import BaseLayout from "@/layouts/base";
+import { getAllGroupSlugs } from "@/lib/api/groups/getAllGroupSlugs";
+
+export default function group({ data, general }) {
+  console.log(data);
+  return (
+    <BaseLayout pageMeta={data.pageMeta}>
+      <section className="sl-layout">{data.pageTitle}</section>
+    </BaseLayout>
+  );
+}
+
+export async function getStaticPaths() {
+  const { data } = await client.query({
+    query: getAllGroupSlugs(),
+  });
+
+  const paths = data.groups.data.map((group) => {
+    return {
+      params: { slug: group.attributes.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const notFound = { notFound: true };
+  const { params } = context;
+
+  const general = await client.query({
+    query: getGeneralData(),
+  });
+  const groupPage = await client.query({
+    query: getGroupPage(),
+    variables: { slug: params.slug },
+  });
+
+  if (!groupPage?.data?.groups || !general?.data?.generalData) {
+    return notFound;
+  }
+
+  return {
+    props: {
+      data: groupPage.data.groups.data[0].attributes,
+      general: general.data.generalData.data.attributes,
+    },
+  };
+}
