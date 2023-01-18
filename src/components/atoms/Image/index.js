dca100250d984026bb05970bd710c93d7f04e186
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-01-18 13:40:03 +0100

    Add image component

--- src/components/atoms/Image/index.js
+++ src/components/atoms/Image/index.js
@@ -1,25 +1,27 @@
-import NextImage from "next/image";
+import classNames from "classnames";
+import styles from "./Image.module.scss";
 
-// to use this properly, the encompasing div needs a relative style attribute and a size attribute
-const Image = ({ src, styling }) => {
-  if (!src) return <>not a valid image</>;
+const SLImage = ({ data, className }) => {
+  if (!data.url) {
+    return "Image is not valid";
+  }
+
+  const imageClassNames = classNames([styles["image"], className]);
 
   return (
-    <>
-      <NextImage
-        loader={Loader}
-        src={src}
-        quality={100}
-        layout="fill"
-        className={styling}
-        alt="No valid image"
+    <picture className={imageClassNames}>
+      <source media="(max-width: 480px)" srcSet={data?.formats.small.url} />
+      <source media="(max-width: 768px)" srcSet={data?.formats.medium.url} />
+      <source media="(max-width: 1024px)" srcSet={data?.formats.large.url} />
+      <img
+        alt={data?.alternativeText}
+        src={data?.url}
+        srcSet={data?.formats.large.url}
+        sizes={`(max-width: 480px) ${data.formats.small.width}, (max-width: 768px) ${data.formats.medium.width}, ${data?.formats.large.url}`}
+        loading="lazy"
       />
-    </>
+    </picture>
   );
 };
 
-const Loader = ({ src, width, quality }) => {
-  return `${src}?w=${width}`;
-};
-
-export default Image;
+export default SLImage;
