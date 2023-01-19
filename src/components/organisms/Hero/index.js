b6082e555498cd8401a1496dcd881af3b9be81a0
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-01-19 17:51:00 +0100

    Tweak hero

--- src/components/organisms/Hero/index.js
+++ src/components/organisms/Hero/index.js
@@ -1,6 +1,7 @@
-import Link from "@/components/atoms/Link";
-import Image from "@/components/atoms/Image";
+import classNames from "classnames";
 import Title from "@/components/atoms/Title";
+import Button from "@/components/atoms/Button";
+import styles from "./Hero.module.scss";
 
 const Hero = ({
   title,
@@ -10,11 +11,19 @@ const Hero = ({
   socialsCta,
   yearTheme,
   bgImage,
+  className,
 }) => {
+  const heroClassname = classNames([styles["hero"], className]);
+
   return (
-    <>
-      <Title title={title} variant="h1" />
-    </>
+    <div className={heroClassname}>
+      <Title title={title} variant="h1" modLight modMarkup />
+      {subtitle && <Title title={subtitle} style="h3" tagName="p" modAccent />}
+      <div className={styles["hero__buttons"]}>
+        <Button label="Het jaarthema" />
+        <Button label="Inschrijven" variant="light" />
+      </div>
+    </div>
   );
 };
 
