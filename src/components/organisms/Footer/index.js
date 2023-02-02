079b39ade8eacb2fec34c556ae1afb806a426644
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-02-02 16:40:35 +0100

    Fix layout container for footer

--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -4,25 +4,25 @@ import FooterHead from "@/components/molecules/FooterHead";
 import FooterBottom from "@/components/molecules/FooterBottom";
 import FooterInfo from "@/components/molecules/FooterInfo";
 import styles from "./Footer.module.scss";
-import classNames from "classnames";
 
 const Footer = () => {
   const { general } = useContext(GeneralContext);
-  const footerClassNames = classNames([styles["footer"], "sl-layout"]);
 
   return (
-    <footer className={footerClassNames}>
-      <FooterHead
-        siteName={general?.siteName}
-        vatNumber={general?.vatNumber}
-        groupNumber={general?.groupNumber}
-      />
-      <FooterInfo
-        address={general?.address}
-        contactItems={general?.contactItems}
-        footerNavigation={general?.footerNavigation}
-      />
-      <FooterBottom siteName={general?.siteName} />
+    <footer className={styles["footer"]}>
+      <div className="sl-layout">
+        <FooterHead
+          siteName={general?.siteName}
+          vatNumber={general?.vatNumber}
+          groupNumber={general?.groupNumber}
+        />
+        <FooterInfo
+          address={general?.address}
+          contactItems={general?.contactItems}
+          footerNavigation={general?.footerNavigation}
+        />
+        <FooterBottom siteName={general?.siteName} />
+      </div>
     </footer>
   );
 };
