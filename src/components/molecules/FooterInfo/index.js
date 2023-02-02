c9c48febc49ca9602e2e38352965809aa1c59f25
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-02-02 16:26:14 +0100

    Add simple footer

---
+++ src/components/molecules/FooterInfo/index.js
@@ -0,0 +1,44 @@
+import Link from "@/components/atoms/Link";
+import Title from "@/components/atoms/Title";
+import { Markup } from "interweave";
+import styles from "./FooterInfo.module.scss";
+
+const InfoCol = ({ title, address, links }) => {
+  return (
+    <div className={styles["info-col"]}>
+      <Title title={title} variant="h3" className={styles["info-col__title"]} />
+
+      {address ? (
+        <div className={styles["info-col__content"]}>
+          <Markup content={address} />
+        </div>
+      ) : (
+        links?.map((item, i) => {
+          return (
+            <Link key={i} href={item.link || "#"}>
+              <span className={styles["info-col__content"]}>{item.label}</span>
+            </Link>
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+const FooterInfo = ({ address, contactItems, footerNavigation }) => {
+  return (
+    <div className={styles["footer-info"]}>
+      <div className={styles["footer-info__section"]}>
+        <InfoCol title="Adres" address={address} />
+        <InfoCol title="Contact" links={contactItems} />
+      </div>
+      <div className={styles["footer-info__section"]}>
+        {footerNavigation.map((el, i) => {
+          return <InfoCol key={i} title={el.title} links={el.navItems} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FooterInfo;
--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -1,5 +1,30 @@
-const Footer = ({ footInfo, socials, address }) => {
-  return <footer className="sl-layout">Footer</footer>;
+import { useContext } from "react";
+import { GeneralContext } from "@/context/GeneralContext";
+import FooterHead from "@/components/molecules/FooterHead";
+import FooterBottom from "@/components/molecules/FooterBottom";
+import FooterInfo from "@/components/molecules/FooterInfo";
+import styles from "./Footer.module.scss";
+import classNames from "classnames";
+
+const Footer = () => {
+  const { general } = useContext(GeneralContext);
+  const footerClassNames = classNames([styles["footer"], "sl-layout"]);
+
+  return (
+    <footer className={footerClassNames}>
+      <FooterHead
+        siteName={general?.siteName}
+        vatNumber={general?.vatNumber}
+        groupNumber={general?.groupNumber}
+      />
+      <FooterInfo
+        address={general?.address}
+        contactItems={general?.contactItems}
+        footerNavigation={general?.footerNavigation}
+      />
+      <FooterBottom siteName={general?.siteName} />
+    </footer>
+  );
 };
 
 export { Footer };
