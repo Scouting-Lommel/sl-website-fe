bb9ca61c79c22e4f6adf4c87da724fe523d6757b
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-02-10 22:09:04 +0100

    Add small navigation

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import classNames from 'classnames';
 import { GeneralContext } from '@/context/GeneralContext';
 import Navigation from '@/components/molecules/Navigation';
 import SLImage from '@/components/atoms/Image';
@@ -7,6 +8,15 @@ import styles from './Header.module.scss';
 
 const Header = () => {
   const { general } = useContext(GeneralContext);
+  const [navVisible, setNavVisible] = useState(false);
+  const navClassnames = classNames(
+    styles['header__nav'],
+    navVisible ? styles['header__nav--visible'] : styles['header__nav--invisible'],
+  );
+
+  const triggerNav = () => {
+    setNavVisible(!navVisible);
+  };
 
   return (
     <div className={styles['header__wrapper']}>
@@ -16,7 +26,12 @@ const Header = () => {
             <Link href="/" className={styles['header__link']}>
               <SLImage data={general.generalData.data.attributes.logo.data.attributes} />
             </Link>
-            <Navigation navItems={general.generalData.data.attributes.mainNavigation} />
+            <div className={navClassnames}>
+              <Navigation navItems={general.generalData.data.attributes.mainNavigation} />
+            </div>
+            <button className={styles['header__trigger']} onClick={() => triggerNav()}>
+              =
+            </button>
           </div>
         </header>
       </div>
