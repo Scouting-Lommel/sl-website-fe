fd4b654ab90325c7b5fc93959ce5a70045b4d37c
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-03-07 16:38:12 +0100

    Add sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+const { withSentryConfig } = require('@sentry/nextjs');
+
 const nextTranslate = require('next-translate');
 const { locales, defaultLocale } = require('./src/locales');
 
@@ -31,3 +37,5 @@ module.exports = nextTranslate({
     return [{ source: '/home', destination: '/', permanent: true }];
   },
 });
+
+module.exports = withSentryConfig(module.exports, { silent: true }, { hideSourcemaps: true });
