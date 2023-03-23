e71e4f8d6943bfe5b0438922abe1006f43b7d6bd
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-03-23 16:17:34 +0100

    Add button story

---
+++ src/components/atoms/Button/Button.stories.js
@@ -0,0 +1,20 @@
+import Button from '.';
+
+const ButtonStory = {
+  title: 'Atoms/Button',
+  component: Button,
+};
+
+export default ButtonStory;
+
+const Template = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "I'm a button!",
+  variant: 'primary',
+  type: 'button',
+  modLink: false,
+  modSmall: false,
+  href: '/#',
+};
