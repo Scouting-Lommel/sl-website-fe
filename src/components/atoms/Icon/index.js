f8303f9bce3002aa562030c473d341785c97752e
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-04-25 22:23:02 +0200

    Fix icon proptype

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -25,7 +25,7 @@ const Icon = ({ modBlock, modInline, icon, title, className }) => {
 };
 
 Icon.propTypes = {
-  icon: PropTypes.func,
+  icon: PropTypes.elementType,
   modInline: PropTypes.bool,
   modBlock: PropTypes.bool,
   title: PropTypes.string,
