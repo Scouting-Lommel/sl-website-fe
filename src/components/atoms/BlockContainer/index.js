09e7b134c84606aae311ed7ff151bcdc070312c5
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-05-03 09:53:33 +0200

    Add block container v1

--- src/components/atoms/BlockContainer/index.js
+++ src/components/atoms/BlockContainer/index.js
@@ -4,11 +4,11 @@ import CallToAction from '@/components/molecules/CallToAction';
 import styles from './BlockContainer.module.scss';
 
 const BlockContainer = ({ variant, orientation, cta, slug, bgImage, modCtaSocials, children }) => {
-  const containerClassnames = classNames([
-    styles['block-container'],
-    styles[`block-container--${variant}`],
-    styles[`block-container--${orientation}`],
-    bgImage && styles[`block-container--opaque`],
+  const bgClassnames = classNames([
+    styles['block-container__bg-image'],
+    styles[`block-container__bg-image--${variant}`],
+    styles[`block-container__bg-image--${orientation}`],
+    bgImage && styles[`block-container__bg-image--opaque`],
   ]);
 
   const ctaClassnames = classNames([
@@ -18,20 +18,18 @@ const BlockContainer = ({ variant, orientation, cta, slug, bgImage, modCtaSocial
   ]);
 
   return (
-    <section id={slug} className={containerClassnames}>
-      {bgImage && (
-        <>
-          <picture className={styles['block-container__bg-image']}>
-            <img
-              className={styles['image__img']}
-              alt={bgImage?.alternativeText}
-              src={bgImage?.url}
-              sizes="100vw"
-              loading="eager"
-            />
-          </picture>
-        </>
-      )}
+    <section id={slug} className={styles['block-container']}>
+      <div className={bgClassnames}>
+        {bgImage && (
+          <img
+            className={styles['image__img']}
+            alt={bgImage?.alternativeText}
+            src={bgImage?.url}
+            sizes="100vw"
+            loading="eager"
+          />
+        )}
+      </div>
       <div className={styles['block-container__content']}>{children}</div>
       {cta && (
         <CallToAction
