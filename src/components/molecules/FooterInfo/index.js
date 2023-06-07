b53620b6c901e79e5c14767312caf0f2387e1ea8
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-06-07 10:36:30 +0200

    Remove next-translate

--- src/components/molecules/FooterInfo/index.js
+++ src/components/molecules/FooterInfo/index.js
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import useTranslation from 'next-translate/useTranslation';
 import Typography from '@/components/atoms/Typography';
 import Title from '@/components/atoms/Title';
 import SLLink from '@/components/atoms/Link';
@@ -36,13 +35,11 @@ const InfoCol = ({ title, address, links }) => {
 };
 
 const FooterInfo = ({ address, contactItems, footerNavigation }) => {
-  const { t } = useTranslation('common');
-
   return (
     <div className={styles['footer-info']}>
       <div className={styles['footer-info__section']}>
-        <InfoCol title={t('Footer.Address')} address={address} />
-        <InfoCol title={t('Footer.Contact')} links={contactItems} />
+        <InfoCol title="Adres" address={address} />
+        <InfoCol title="Contact" links={contactItems} />
       </div>
       <div className={styles['footer-info__section']}>
         {footerNavigation.map((el, i) => {
--- src/locales/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  locales: ['nl'],
-  defaultLocale: 'nl',
-};
