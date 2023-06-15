b8b4cab56f515138976fac4ee142b29b33083ac9
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-06-15 16:07:23 +0200

    Add YearTheme styles

--- src/components/molecules/YearTheme/index.tsx
+++ src/components/molecules/YearTheme/index.tsx
@@ -1,16 +1,20 @@
 import classNames from 'classnames';
 import SLImage from '@/components/atoms/Image';
 import { YearTheme as YearThemeProps } from './types';
-import styles from './YearTheme.module.scss';
+import styles from './YearTheme.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type Props = YearThemeProps & React.HTMLAttributes<HTMLElement>;
 
 const YearTheme = ({ image, className }: Props) => {
-  const yearThemeClassnames = classNames([styles['year-theme'], className]);
+  const yearThemeClassnames = classNames('year-theme', className);
 
   return (
     <div className={yearThemeClassnames}>
-      <SLImage data={image} className={styles['year-theme__image']} loadingStrategy={'lazy'} />
+      <SLImage data={image} className="year-theme__image" loadingStrategy="lazy" />
     </div>
   );
 };
