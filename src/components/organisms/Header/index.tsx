019082fb55e319c2fe71a5795d86f33c36ebac0f
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-06-16 08:57:26 +0200

    Add Header styles

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -8,15 +8,19 @@ import Icon from '@/components/atoms/Icon';
 import Navigation from '@/components/molecules/Navigation';
 import SLImage from '@/components/atoms/Image';
 import { Header as HeaderProps } from './types';
-import styles from './Header.module.scss';
+import styles from './Header.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type Props = HeaderProps & React.HTMLAttributes<HTMLElement>;
 
 const Header = ({ logo, mainNavigation, groups, rentalLocations }: Props) => {
   const [navVisible, setNavVisible] = useState(false);
   const navClassnames = classNames(
-    styles['header__nav'],
-    navVisible ? styles['header__nav--visible'] : styles['header__nav--invisible'],
+    'header__nav',
+    navVisible ? 'header__nav--visible' : 'header__nav--invisible',
   );
 
   const triggerNav = () => {
@@ -24,11 +28,11 @@ const Header = ({ logo, mainNavigation, groups, rentalLocations }: Props) => {
   };
 
   return (
-    <div className={styles['header__wrapper']}>
-      <div className={styles['header']}>
+    <div className="header__wrapper">
+      <div className="header">
         <header className="sl-layout">
-          <div className={styles['header__content']}>
-            <Link href="/" className={styles['header__link']}>
+          <div className="header__content">
+            <Link href="/" className="header__link">
               <SLImage data={logo.data.attributes} loadingStrategy="lazy" />
             </Link>
             <div className={navClassnames}>
@@ -38,7 +42,7 @@ const Header = ({ logo, mainNavigation, groups, rentalLocations }: Props) => {
                 rentalLocations={rentalLocations}
               />
             </div>
-            <button className={styles['header__trigger']} onClick={() => triggerNav()}>
+            <button className="header__trigger" onClick={() => triggerNav()}>
               {navVisible ? (
                 <Icon icon={IconClose} size="lg" title="Close menu" />
               ) : (
