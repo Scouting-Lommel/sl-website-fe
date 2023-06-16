96d960226c68377e235714e83d01d0b9b3753468
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-06-16 08:47:15 +0200

    Add CallToAction styles

--- src/components/molecules/CallToAction/index.tsx
+++ src/components/molecules/CallToAction/index.tsx
@@ -1,16 +1,20 @@
 import classNames from 'classnames';
 import Button from '@/components/atoms/Button';
 import { CallToAction as CallToActionProps } from './types';
-import styles from './CallToAction.module.scss';
+import styles from './CallToAction.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type Props = CallToActionProps & React.HTMLAttributes<HTMLElement>;
 
 const CallToAction = ({ title, intro, ctaLabel, ctaLink, className }: Props) => {
   return (
-    <div className={classNames([styles['cta'], className])}>
-      <div className={styles['cta__copy']}>
-        <div className={styles['cta__copy__title']}>{title}</div>
-        <div className={styles['cta__copy__intro']}>{intro}</div>
+    <div className={classNames('cta', className)}>
+      <div className="cta__copy">
+        <div className="cta__copy__title">{title}</div>
+        <div className="cta__copy__intro">{intro}</div>
       </div>
       {ctaLabel && ctaLink && (
         <Button label={ctaLabel} href={ctaLink || ''} variant="primary" modSmall />
