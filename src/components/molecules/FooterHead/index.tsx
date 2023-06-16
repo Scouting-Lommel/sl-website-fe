2763b6e169bf0c9deb4330935d4719dc9bf34c5b
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-06-16 08:01:43 +0200

    Add FooterHead styles

--- src/components/molecules/FooterHead/index.tsx
+++ src/components/molecules/FooterHead/index.tsx
@@ -1,13 +1,17 @@
 import { FooterHead as FooterHeadProps } from './types';
-import styles from './FooterHead.module.scss';
+import styles from './FooterHead.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type Props = FooterHeadProps & React.HTMLAttributes<HTMLElement>;
 
 const FooterHead = ({ siteName, vatNumber, groupNumber }: Props) => {
   return (
-    <div className={styles['footer-head']}>
-      <h2 className={styles['footer-head__title']}>{siteName}</h2>
-      <p className={styles['footer-head__info']}>
+    <div className="footer-head">
+      <h2 className="footer-head__title">{siteName}</h2>
+      <p className="footer-head__info">
         {vatNumber} • {groupNumber}
       </p>
     </div>
