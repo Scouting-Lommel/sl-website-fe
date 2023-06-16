69caaa4f16a91516961062413e2bfed12bbeed2f
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-06-16 08:13:45 +0200

    Add FooterDoormat styles

--- src/components/molecules/FooterDoormat/index.tsx
+++ src/components/molecules/FooterDoormat/index.tsx
@@ -1,19 +1,22 @@
-import classNames from 'classnames';
 import Typography from '@/components/atoms/Typography';
 import SLLink from '@/components/atoms/Link';
 import { FooterDoormat as FooterDoormatProps, DoormatCol as DoormatColProps } from './types';
-import styles from './FooterDoormat.module.scss';
+import styles from './FooterDoormat.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type DoormatProps = FooterDoormatProps & React.HTMLAttributes<HTMLElement>;
 type ColProps = DoormatColProps & React.HTMLAttributes<HTMLElement>;
 
 const DoormatCol = ({ title, address, links }: ColProps) => {
   return (
-    <div className={styles['doormat-col']}>
-      <h3 className={styles['doormat-col__title']}>{title}</h3>
+    <div className="doormat-col">
+      <h3 className="doormat-col__title">{title}</h3>
 
       {address ? (
-        <div className={styles['doormat-col__content']}>
+        <div className="doormat-col__content">
           <Typography data={address} modNoStyle />
         </div>
       ) : (
@@ -23,10 +26,7 @@ const DoormatCol = ({ title, address, links }: ColProps) => {
               key={i}
               href={item.link || '#'}
               variant="link1"
-              className={classNames([
-                styles['doormat-col__content'],
-                styles['doormat-col__content--link'],
-              ])}
+              className="doormat-col__content doormat-col__content--link"
             >
               {item.label}
             </SLLink>
@@ -39,12 +39,12 @@ const DoormatCol = ({ title, address, links }: ColProps) => {
 
 const FooterDoormat = ({ address, contactItems, footerNavigation }: DoormatProps) => {
   return (
-    <div className={styles['footer-doormat']}>
-      <div className={styles['footer-doormat__section']}>
+    <div className="footer-doormat">
+      <div className="footer-doormat__section">
         <DoormatCol title="Adres" address={address} />
         <DoormatCol title="Contact" links={contactItems} />
       </div>
-      <div className={styles['footer-doormat__section']}>
+      <div className="footer-doormat__section">
         {footerNavigation.map((el, i) => {
           return <DoormatCol key={i} title={el.title} links={el.navItems} />;
         })}
