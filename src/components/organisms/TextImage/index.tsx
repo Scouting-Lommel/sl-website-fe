3e415ce0efb931e0f2674c2abe13c763b7f97836
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-07-21 22:36:00 +0200

    Add TextImage styles

--- src/components/organisms/TextImage/index.tsx
+++ src/components/organisms/TextImage/index.tsx
@@ -1,28 +1,41 @@
+// TODO: add variant for multiple images
+// Figma: https://www.figma.com/file/hBMZQRv8UQUPIzGJ4A9uWm/Scouting-Lommel?type=design&node-id=7920-1220&mode=design&t=2HYlIEH4iYdbOht6-4
+
+import classNames from 'classnames';
 import Typography from '@/components/atoms/Typography';
 import { TextImage as TextImageProps } from './types';
 import SLImage from '@/components/atoms/Image';
 import Button from '@/components/atoms/Button';
+import styles from './TextImage.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type Props = TextImageProps & React.HTMLAttributes<HTMLElement>;
 
-const ImageText = ({ title, content, images, variant, ctaButton }: Props) => {
+const ImageText = ({ title, content, images, variant, ctaButton, className }: Props) => {
+  const textImageClassname = classNames('text-image', `text-image--${variant}`, className);
+
   return (
-    <div>
-      {images &&
-        images.length > 0 &&
-        images.map((image, i) => {
-          return <SLImage key={i} data={image} loadingStrategy={'lazy'} />;
-        })}
-      <h2>{title}</h2>
-      <Typography modNoStyle data={content} />
-      {ctaButton && (
-        <Button
-          label={ctaButton.label}
-          href={ctaButton.link}
-          variant={ctaButton.variant}
-          modSmall
-        />
-      )}
+    <div className={textImageClassname}>
+      <div className="text-image__image-container">
+        {images && images.length > 0 && (
+          <SLImage data={images[0]} loadingStrategy={'lazy'} className="text-image__image" />
+        )}
+      </div>
+      <div className="text-image__content">
+        <h2>{title}</h2>
+        <Typography modNoStyle data={content} />
+        {ctaButton && (
+          <Button
+            label={ctaButton.label}
+            href={ctaButton.link}
+            variant={ctaButton.variant}
+            className="text-image__content__button"
+          />
+        )}
+      </div>
     </div>
   );
 };
