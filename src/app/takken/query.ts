e1c0f4c4c4827bcf8f19b10ac64532c673ed22a6
Seppe Alaerts <seppe.alaerts@craftzing.com>
2023-07-23 12:02:01 +0200

    Add takken queries

--- src/app/takken/query.ts
+++ src/app/takken/query.ts
@@ -1,9 +1,13 @@
 import gql from 'graphql-tag';
 
 import HERO_BLOCK_FRAGMENT from '@/graphql/hero-block.gql';
+import TEXT_IMAGE_BLOCK_FRAGMENT from '@/graphql/text-image-block.gql';
+import GROUPS_BLOCK_FRAGMENT from '@/graphql/groups-block.gql';
 
 const GROUPS_PAGE_QUERY = gql`
-  ${HERO_BLOCK_FRAGMENT}
+  ${HERO_BLOCK_FRAGMENT}    
+  ${TEXT_IMAGE_BLOCK_FRAGMENT}, 
+  ${GROUPS_BLOCK_FRAGMENT}
 
   query {
     groupsPage {
@@ -49,6 +53,8 @@ const GROUPS_PAGE_QUERY = gql`
           blocks {
             __typename
             ...HeroBlockFragment
+            ...TextImageBlockFragment
+            ...GroupsBlockFragment
           }
         }
       }
