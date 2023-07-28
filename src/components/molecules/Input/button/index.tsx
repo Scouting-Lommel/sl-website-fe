0b1c889e0bf4e3092b5ebf07ddb9e355916f95e6
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2023-07-28 21:02:14 +0200

    styling of button and checkbox done

--- src/components/molecules/Input/button/index.tsx
+++ src/components/molecules/Input/button/index.tsx
@@ -1,9 +1,23 @@
 import { ButtonInput as InputProps } from './types';
+import styles from './button.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type Props = InputProps & React.HTMLAttributes<HTMLElement>;
 
 const buttonInput = ({ title, type, placeholder, required, options, redirect }: Props) => {
-  return <input type={type} id={title} value={placeholder} required={required} {...options} />;
+  return (
+    <input
+      className="buttonInput"
+      type={type}
+      id={title}
+      value={placeholder}
+      required={required}
+      {...options}
+    />
+  );
 };
 
 export default buttonInput;
--- src/components/molecules/Input/checkbox/index.tsx
+++ src/components/molecules/Input/checkbox/index.tsx
@@ -1,12 +1,26 @@
 import { CheckboxInput as InputProps } from './types';
+import styles from './checkbox.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type Props = InputProps & React.HTMLAttributes<HTMLElement>;
 
 const checkboxInput = ({ title, type, placeholder, required, options, redirect }: Props) => {
   return (
     <>
-      <input type={type} id={title} name={options} value={options} required={required} />{' '}
-      <label htmlFor={title}>{options}</label>
+      <label className="checkboxLabel" htmlFor={title}>
+        <input
+          className="checkboxInput"
+          type={type}
+          id={title}
+          name={options}
+          value={options}
+          required={required}
+        />
+        {options}
+      </label>
     </>
   );
 };
