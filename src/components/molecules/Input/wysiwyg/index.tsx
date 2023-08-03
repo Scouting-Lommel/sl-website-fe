1b866586f0c438ec9062e2bf105fa613e087487e
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2023-08-03 11:23:00 +0200

    done different screen sizes

--- src/components/molecules/Input/wysiwyg/index.tsx
+++ src/components/molecules/Input/wysiwyg/index.tsx
@@ -1,4 +1,9 @@
 import { WYSIWYGInput as InputProps } from './types';
+import styles from './wysiwyg.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type Props = InputProps & React.HTMLAttributes<HTMLElement>;
 
