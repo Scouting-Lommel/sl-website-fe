bfde063e99203d25525cc7cd3b6eaae165a3fd12
Vincent <vincent.wijshoff@gmail.com>
2023-09-14 18:43:04 +0200

    more work

--- src/components/organisms/Gallery/index.tsx
+++ src/components/organisms/Gallery/index.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import { Gallery as GalleryProps } from './types';
 import styles from './Gallery.css';
 import SLImage from '@/components/atoms/Image';
+import { useState } from 'react';
+import Button from '@/components/atoms/Button';
 
 export const links = () => {
   return [{ rel: 'stylesheet', href: styles }];
@@ -9,23 +13,34 @@ export const links = () => {
 type Props = GalleryProps & React.HTMLAttributes<HTMLElement>;
 
 const Gallery = ({ title, initialItems, images }: Props) => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <div>
       <h2 className="t-headline-2 t-align-center">{title}</h2>
       <div className="gallery--imagecontainer">
-        {[...Array(initialItems)].map((_, i) => {
-          if (images.data.length > i) {
-            return (
-              <SLImage
-                key={i}
-                data={images.data[i].attributes}
-                loadingStrategy={'lazy'}
-                className="Gallery--image"
-              />
-            );
-          }
-          return <></>;
-        })}
+        {!isOpen &&
+          [...Array(initialItems)].map((_, i) => {
+            if (images.data.length > i) {
+              return (
+                <SLImage
+                  key={i}
+                  data={images.data[i].attributes}
+                  loadingStrategy={'lazy'}
+                  className="Gallery--image"
+                />
+              );
+            }
+            return <></>;
+          })}
+        {isOpen && <></>}
+      </div>
+      <div className="gallery--button">
+        <Button
+          label={isOpen ? "Bekijk minder foto's" : "Bekijk alle foto's"}
+          onClick={() => {
+            setOpen(!isOpen);
+          }}
+        />
       </div>
     </div>
   );
