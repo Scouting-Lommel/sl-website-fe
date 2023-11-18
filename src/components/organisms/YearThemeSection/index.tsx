d5c8b4dcaed321e167f22ee3db885faa2773b2a4
Seppe <seppe.alaerts21@gmail.com>
2023-11-18 13:12:04 +0100

    Add YearThemeSection styles

--- src/components/organisms/YearThemeSection/index.tsx
+++ src/components/organisms/YearThemeSection/index.tsx
@@ -1,17 +1,22 @@
 import Typography from '@/components/atoms/Typography';
 import { YearThemeSection as YearThemeSectionProps } from './types';
 import YearTheme from '@/components/molecules/YearTheme';
+import styles from './YearThemeSection.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 type Props = YearThemeSectionProps & React.HTMLAttributes<HTMLElement>;
 
 const YearThemeSection = ({ yearTheme }: Props) => {
   return (
-    <div>
-      <div>
-        <h2 className="t-headline-2 t-align-center">{yearTheme.title}</h2>
+    <div className="year-theme-section">
+      <h2 className="year-theme-section__content__title t-headline-1">{yearTheme.title}</h2>
+      <div className="year-theme-section__content">
         <Typography data={yearTheme.description} />
+        <YearTheme className="year-theme-section__image" image={yearTheme.image.data.attributes} />
       </div>
-      <YearTheme image={yearTheme.image.data.attributes} />
     </div>
   );
 };
