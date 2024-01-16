0eea130b252c757aa7d4d5d5bb200ed37b1b116f
Seppe <seppe.alaerts21@gmail.com>
2024-01-16 20:04:17 +0100

    Tweak leaders grid

--- src/components/organisms/Leaders/index.tsx
+++ src/components/organisms/Leaders/index.tsx
@@ -11,19 +11,21 @@ type Props = LeadersProps & React.HTMLAttributes<HTMLElement>;
 const Leaders = ({ leaders }: Props) => {
   return (
     <div className="leaders__container">
-      {leaders.map((leader, i) => {
-        if (leader.attributes.active) {
-          return (
-            <Leader
-              active={leader.attributes.active}
-              firstName={leader.attributes.firstName}
-              lastName={leader.attributes.lastName}
-              image={leader.attributes.image}
-              key={i}
-            />
-          );
-        }
-      })}
+      <div className="leaders">
+        {leaders.map((leader, i) => {
+          if (leader.attributes.active) {
+            return (
+              <Leader
+                active={leader.attributes.active}
+                firstName={leader.attributes.firstName}
+                lastName={leader.attributes.lastName}
+                image={leader.attributes.image}
+                key={i}
+              />
+            );
+          }
+        })}
+      </div>
     </div>
   );
 };
