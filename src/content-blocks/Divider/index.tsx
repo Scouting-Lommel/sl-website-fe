67b263a8202caa0c2f7af1b64b1951124985864d
Seppe <seppe.alaerts21@gmail.com>
2024-03-25 16:16:08 +0100

    Remove variant prop from divider

--- src/content-blocks/Divider/index.tsx
+++ src/content-blocks/Divider/index.tsx
@@ -2,7 +2,7 @@ import { Divider as DividerProps } from './types';
 
 type Props = DividerProps & React.HTMLAttributes<HTMLElement>;
 
-const Divider = ({ variant = 'default' }: Props) => {
+const Divider = ({}: Props) => {
   return <hr className="sl-layout" />;
 };
 
