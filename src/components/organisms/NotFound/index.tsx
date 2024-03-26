0b38ac6d3548e58f52935791c0305fe02c59dde8
Seppe Alaerts <45093145+seppealaerts@users.noreply.github.com>
2024-03-26 21:12:47 +0100

    SLWEB-18 | Error pages (#91)
    * Add not found component
    
    * Tweak margin
    
    * Tweak link

---
+++ src/components/organisms/NotFound/index.tsx
@@ -0,0 +1,27 @@
+import BlockContainer from '@/components/atoms/BlockContainer';
+import Hero from '@/components/organisms/Hero';
+import SLLink from '@/components/atoms/Link';
+import Typography from '@/components/atoms/Typography';
+import styles from './NotFound.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
+
+const NotFound = () => {
+  return (
+    <BlockContainer variant="light" orientation="default" slug="page-not-found">
+      <section className="sl-layout">
+        <Hero subtitle="Pagina niet gevonden" title="404" variant="simple" className="sl-layout" />
+        <Typography className="not-found__content">
+          <p>We hebben de pagina die je zocht, niet gevonden.</p>
+          <p>
+            <a href="/">Terug naar de homepagina</a>
+          </p>
+        </Typography>
+      </section>
+    </BlockContainer>
+  );
+};
+
+export default NotFound;
