ca9e18dda3649672f7b94d814af65e0609b38ea0
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-04-18 10:27:00 +0200

    Add formStatus enum

---
+++ src/lib/constants/formStatus.ts
@@ -0,0 +1,7 @@
+export enum FormStatus {
+  STATUS_READY = 'ready',
+  STATUS_LOADING = 'loading',
+  STATUS_SUCCESS = 'success',
+  STATUS_ERROR = 'error',
+  STATUS_CAPTCHA_NOT_VERIFIED = 'not-verified',
+}
