210d1b035c7d63a07a6e028f4abcddb21dbb0dc0
Seppe <seppe.alaerts21@gmail.com>
2024-05-03 08:53:05 +0200

    Tweak input

--- src/components/atoms/Forms/Input/index.tsx
+++ src/components/atoms/Forms/Input/index.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import classNames from 'classnames';
 import Typography from '@/components/atoms/Typography';
 import { FormInput as FormInputProps } from './types';
@@ -9,25 +10,29 @@ export const links = () => {
 
 type Props = FormInputProps & React.InputHTMLAttributes<HTMLElement>;
 
-const Input = ({ id, name, label, type, placeholder, required, autoComplete }: Props) => {
-  const inputClassName = classNames('input', required && 'input--required');
+const Input = forwardRef((props: Props, ref: any) => {
+  const inputClassName = classNames('input', props.required && 'input--required');
 
   return (
     <div className={inputClassName}>
-      <label htmlFor={id}>
-        <Typography className="input__label">{label}</Typography>
+      <label htmlFor={props.id}>
+        <Typography className="input__label">{props.label}</Typography>
       </label>
       <input
         className="input__field"
-        id={id}
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        required={required}
-        autoComplete={autoComplete}
+        {...props}
+        ref={ref}
+        id={props.id}
+        name={props.name}
+        type={props.type}
+        placeholder={props.placeholder}
+        required={props.required}
+        autoComplete={props.autoComplete}
       />
     </div>
   );
-};
+});
+
+Input.displayName = 'Input';
 
 export default Input;
