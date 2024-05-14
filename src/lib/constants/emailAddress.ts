20ea885ff4c05c1fd7b1f286460aeae2e5f9227d
Seppe <seppe.alaerts21@gmail.com>
2024-05-14 09:33:31 +0200

    Add email address constants

---
+++ src/lib/constants/emailAddress.ts
@@ -0,0 +1,5 @@
+const noreplyEmailAddress = `"Scouting Lommel Mail" <$noreply@scoutinglommel.be>`;
+const generalEmailAddress = 'info@scoutinglommel.be';
+const rentalsEmailAddress = 'verhuur@scoutinglommel.be';
+
+export { noreplyEmailAddress, generalEmailAddress, rentalsEmailAddress };
