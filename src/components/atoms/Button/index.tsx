79bc135ece3847f2bcd4917ed63d86c8a3cbabfb
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-06-23 00:00:31 +0200

    Add spinner to button when loading

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
 import NextLink from 'next/link';
+import { IconSpinner } from '@/assets/icons';
+import Icon from '@/components/atoms/Icon';
 import { Button as ButtonProps } from './types';
 import styles from './Button.css';
 
@@ -14,6 +16,7 @@ const Button = ({
   variant = 'primary',
   href,
   modSmall,
+  loading,
   className,
   children,
   ...props
@@ -28,13 +31,18 @@ const Button = ({
   if (href) {
     return (
       <NextLink className={buttonClassNames} href={href} {...props}>
-        <span className="button__label">{children || label}</span>
+        <span className="button__label">
+          {children || label}
+          {loading && <Icon icon={IconSpinner} title="Laden..." size="xs" />}
+        </span>
       </NextLink>
     );
   }
   return (
     <button className={buttonClassNames} {...props}>
-      <span className="button__label">{children || label}</span>
+      <span className="button__label">
+        {children || label} {loading && <Icon icon={IconSpinner} title="Laden..." size="xs" />}
+      </span>
     </button>
   );
 };
