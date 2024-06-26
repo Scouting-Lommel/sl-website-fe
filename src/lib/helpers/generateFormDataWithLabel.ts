14e5504ecdcbc609349c220273c72ce43bd7cc19
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-06-26 12:01:32 +0200

    Filter out non-form field elements from form submissions

--- src/lib/helpers/generateFormDataWithLabel.ts
+++ src/lib/helpers/generateFormDataWithLabel.ts
@@ -1,29 +1,42 @@
 import { FormField } from '@/components/organisms/Forms/FormBuilder/FormField/types';
 
-const findFieldLabelByName = (formFields: any, name: string) => {
+const findFieldByName = (formFields: any, name: string) => {
   for (let i = 0; i < formFields.length; i++) {
     const field = formFields[i];
 
     if (field.name === name) {
-      return field.label;
+      return field;
     }
 
     if (field.type === 'row') {
       for (let j = 0; j < field.fieldChildren.length; j++) {
         if (field.fieldChildren[j].name === name) {
-          return field.fieldChildren[j].label;
+          return field.fieldChildren[j];
         }
       }
     }
   }
+};
 
-  return name;
+const findFieldLabel = (formFields: any, name: string) => {
+  const field = findFieldByName(formFields, name);
+
+  return field.label || name;
+};
+
+const isFormField = (formFields: any, name: string) => {
+  const field = findFieldByName(formFields, name);
+
+  return !['row', 'divider', 'text', 'captcha'].includes(field.type);
 };
 
 const generateFormDataWithLabel = (formData: any, formFields: FormField[]) => {
-  return Object.keys(formData || {}).map((dataKey: any) => {
-    return { label: findFieldLabelByName(formFields, dataKey), data: formData[dataKey] };
-  });
+  return Object.keys(formData || {})
+    .map((dataKey: any) => {
+      if (!isFormField(formFields, dataKey)) return;
+      return { label: findFieldLabel(formFields, dataKey), data: formData[dataKey] };
+    })
+    .filter((el) => el);
 };
 
 export default generateFormDataWithLabel;
