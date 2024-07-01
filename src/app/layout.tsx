0ed00509eca33c690e0f5a98c97848b8167ceb6f
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-07-01 14:50:47 +0200

    Add sessionprovider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 export const dynamic = 'force-dynamic';
 
+import { getServerSession } from 'next-auth';
+import SessionProvider from '@/lib/providers/SessionProvider';
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import { generateMetadataForRootLayout } from '@/lib/helpers/generateMetadata';
 import Header from '@/components/organisms/Header';
 import SkipToContent from '@/components/atoms/SkipToContent';
@@ -18,28 +21,31 @@ export async function generateMetadata() {
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const data = await getGeneralData();
+  const session = await getServerSession(authOptions);
 
   return (
     <html lang="nl">
       <body>
-        <SkipToContent className="skip-to-content" />
-        <Header
-          logo={data.generalData.data.attributes.logo}
-          mainNavigation={data.generalData.data.attributes.mainNavigation}
-          groups={data.groups.data.map((item: any) => item.attributes)}
-          rentalLocations={data.rentalLocations.data.map((item: any) => item.attributes)}
-        />
-        <main className="sl-main" id="main">
-          {children}
-        </main>
-        <Footer
-          siteName={data.generalData.data.attributes.siteName}
-          vatNumber={data.generalData.data.attributes.vatNumber}
-          groupNumber={data.generalData.data.attributes.groupNumber}
-          address={data.generalData.data.attributes.address}
-          contactItems={data.generalData.data.attributes.contactItems}
-          footerNavigation={data.generalData.data.attributes.footerNavigation}
-        />
+        <SessionProvider session={session}>
+          <SkipToContent className="skip-to-content" />
+          <Header
+            logo={data.generalData.data.attributes.logo}
+            mainNavigation={data.generalData.data.attributes.mainNavigation}
+            groups={data.groups.data.map((item: any) => item.attributes)}
+            rentalLocations={data.rentalLocations.data.map((item: any) => item.attributes)}
+          />
+          <main className="sl-main" id="main">
+            {children}
+          </main>
+          <Footer
+            siteName={data.generalData.data.attributes.siteName}
+            vatNumber={data.generalData.data.attributes.vatNumber}
+            groupNumber={data.generalData.data.attributes.groupNumber}
+            address={data.generalData.data.attributes.address}
+            contactItems={data.generalData.data.attributes.contactItems}
+            footerNavigation={data.generalData.data.attributes.footerNavigation}
+          />
+        </SessionProvider>
       </body>
     </html>
   );
