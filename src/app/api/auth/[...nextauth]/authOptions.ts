af6648cea541c9adb4d8dff6488804288a529067
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-07-10 17:59:39 +0200

    Add get user groups callback

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -8,4 +8,24 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
+
+  callbacks: {
+    async signIn({ user }) {
+      const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/get-user-groups`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: user.email }),
+      });
+
+      const groups = await res.json();
+
+      console.log(groups);
+
+      return true;
+    },
+    async session({ session, token }) {
+      const newSession = { ...session, user: { ...session.user, groups: token.groups } };
+      return newSession;
+    },
+  },
 };
