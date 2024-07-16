21f87fc92e55da30dfd8cdfc40849ff60d1ce9bd
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-07-16 21:27:18 +0200

    Extend groups middleware for group detail page

--- src/middlewares/groupsMiddleware.ts
+++ src/middlewares/groupsMiddleware.ts
@@ -1,12 +1,26 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
+import { checkOrganisationPermission } from '@/lib/helpers/checkOrganisationPermission';
 
 export async function groupsMiddleware(req: NextRequest) {
   const token = await getToken({ req });
+  const url: string = req.nextUrl.pathname;
+
+  const signInRedirect = NextResponse.redirect(`${process.env.SITE_URL}/api/auth/signin`);
+  const unauthorizedRedirect = NextResponse.redirect(`${process.env.SITE_URL}/geen-toegang`);
+
+  const getGroup = () => {
+    const regex = new RegExp(`/dashboard/takken/(.*)`);
+    const match = url.match(regex);
+    if (!match) {
+      return null;
+    }
+    return match[1];
+  };
 
   if (!token) {
-    return NextResponse.redirect(`${process.env.SITE_URL}/api/auth/signin`);
+    return signInRedirect;
   }
 
   try {
@@ -16,24 +30,28 @@ export async function groupsMiddleware(req: NextRequest) {
 
     const contentType = response.headers.get('content-type');
     if (!response.ok) {
-      return NextResponse.redirect(`${process.env.SITE_URL}/geen-toegang`);
+      return unauthorizedRedirect;
     }
 
     if (!contentType || !contentType.includes('application/json')) {
-      return NextResponse.redirect(`${process.env.SITE_URL}/geen-toegang`);
+      return unauthorizedRedirect;
     }
 
     const data = await response.json();
 
-    if (!data.orgUnitPath.startsWith('/Leiding') && data.orgUnitPath !== '/') {
-      return NextResponse.redirect(`${process.env.SITE_URL}/geen-toegang`);
+    if (getGroup() && checkOrganisationPermission(data.orgUnitPath, `groups:${getGroup()}`)) {
+      return NextResponse.next();
+    }
+
+    if (!getGroup() && checkOrganisationPermission(data.orgUnitPath, 'groups')) {
+      return NextResponse.next();
     }
   } catch (error: any) {
     console.error(`Error fetching org unit data: ${error.message}`);
-    return NextResponse.redirect(`${process.env.SITE_URL}/geen-toegang`);
+    return unauthorizedRedirect;
   }
 
-  return NextResponse.next();
+  return unauthorizedRedirect;
 }
 
-export const groupsMiddlewareConfig = ['/dashboard/takken'];
+export const groupsMiddlewareConfig = ['/dashboard/takken', '/dashboard/takken/(.*)'];
