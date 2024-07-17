70829430d3417203727d0648717cbb6d8e7ab0ef
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-07-17 21:10:22 +0200

    Add danger button variant

--- src/components/atoms/Button/types.ts
+++ src/components/atoms/Button/types.ts
@@ -1,7 +1,7 @@
 export type Button = {
   [x: string]: any;
   label?: string;
-  variant?: 'primary' | 'light' | 'link1';
+  variant?: 'primary' | 'light' | 'link1' | 'danger';
   href?: string;
   modSmall?: boolean;
 };
