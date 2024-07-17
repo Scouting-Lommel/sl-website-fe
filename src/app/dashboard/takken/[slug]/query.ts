7c5a1a5824fc13a998f0646c8c620840b4b0627e
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-07-17 17:02:04 +0200

    Add activity query mutations

--- src/app/dashboard/takken/[slug]/query.ts
+++ src/app/dashboard/takken/[slug]/query.ts
@@ -43,5 +43,90 @@ const ACTIVITIES_QUERY = gql`
   }
 `;
 
-export { ACTIVITIES_QUERY };
+const CREATE_ACTIVITY_MUTATION = gql`
+  mutation createActivity(
+    $title: String!
+    $startDate: Date!
+    $startTime: Time!
+    $endDate: Date!
+    $endTime: Time!
+    $description: String!
+  ) {
+    createActivity(
+      data: {
+        title: $title
+        startDate: $startDate
+        startTime: $startTime
+        endDate: $endDate
+        endTime: $endTime
+        description: $description
+      }
+    ) {
+      data {
+        id
+        attributes {
+          title
+          startDate
+          startTime
+          endDate
+          endTime
+          description
+        }
+      }
+    }
+  }
+`;
+
+const UPDATE_ACTIVITY_MUTATION = gql`
+  mutation updateActivity(
+    $id: ID!
+    $title: String!
+    $startDate: Date!
+    $startTime: Time!
+    $endDate: Date!
+    $endTime: Time!
+    $description: String!
+  ) {
+    updateActivity(
+      id: $id
+      data: {
+        title: $title
+        startDate: $startDate
+        startTime: $startTime
+        endDate: $endDate
+        endTime: $endTime
+        description: $description
+      }
+    ) {
+      data {
+        id
+        attributes {
+          title
+          startDate
+          startTime
+          endDate
+          endTime
+          description
+        }
+      }
+    }
+  }
+`;
+
+const DELETE_ARTICLE_MUTATION = gql`
+  mutation deleteArticle($id: ID!) {
+    deleteArticle(id: $id) {
+      article {
+        id
+      }
+    }
+  }
+`;
+
+export {
+  ACTIVITIES_QUERY,
+  CREATE_ACTIVITY_MUTATION,
+  UPDATE_ACTIVITY_MUTATION,
+  DELETE_ARTICLE_MUTATION,
+};
 export default GROUP_PAGE_QUERY;
