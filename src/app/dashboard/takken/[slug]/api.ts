325cf80ea170ac4fbaee8b59817dd330ab9ed54f
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-07-17 19:43:29 +0200

    Move activity mutations to lib/api

--- src/app/dashboard/takken/[slug]/api.ts
+++ src/app/dashboard/takken/[slug]/api.ts
@@ -1,10 +1,5 @@
 import { generateApiQuery } from '@/lib/api';
-import GROUP_PAGE_QUERY, {
-  ACTIVITIES_QUERY,
-  CREATE_ACTIVITY_MUTATION,
-  DELETE_ARTICLE_MUTATION,
-  UPDATE_ACTIVITY_MUTATION,
-} from './query';
+import GROUP_PAGE_QUERY, { ACTIVITIES_QUERY } from './query';
 
 export function getGroupPage(slug: string): Promise<any> {
   return generateApiQuery({
@@ -17,41 +12,5 @@ export function getActivities(slug: string, date: string): Promise<any> {
   return generateApiQuery({
     variables: { slug: slug, currDate: date },
     query: ACTIVITIES_QUERY,
-  });
-}
-
-export function createActivity(
-  title: string,
-  startDate: string,
-  startTime: string,
-  endDate: string,
-  endTime: string,
-  description: string,
-): Promise<any> {
-  return generateApiQuery({
-    variables: { title, startDate, startTime, endDate, endTime, description },
-    query: CREATE_ACTIVITY_MUTATION,
-  });
-}
-
-export function updateActivity(
-  id: string,
-  title: string,
-  startDate: string,
-  startTime: string,
-  endDate: string,
-  endTime: string,
-  description: string,
-): Promise<any> {
-  return generateApiQuery({
-    variables: { id, title, startDate, startTime, endDate, endTime, description },
-    query: UPDATE_ACTIVITY_MUTATION,
-  });
-}
-
-export function deleteArticle(id: string): Promise<any> {
-  return generateApiQuery({
-    variables: { id },
-    query: DELETE_ARTICLE_MUTATION,
   });
 }
---
+++ src/lib/api/activities/api.ts
@@ -0,0 +1,42 @@
+import { generateApiQuery } from '@/lib/api';
+import {
+  CREATE_ACTIVITY_MUTATION,
+  DELETE_ARTICLE_MUTATION,
+  UPDATE_ACTIVITY_MUTATION,
+} from './mutations';
+
+export function createActivity(
+  title: string,
+  startDate: string,
+  startTime: string,
+  endDate: string,
+  endTime: string,
+  description: string,
+): Promise<any> {
+  return generateApiQuery({
+    variables: { title, startDate, startTime, endDate, endTime, description },
+    query: CREATE_ACTIVITY_MUTATION,
+  });
+}
+
+export function updateActivity(
+  id: string,
+  title: string,
+  startDate: string,
+  startTime: string,
+  endDate: string,
+  endTime: string,
+  description: string,
+): Promise<any> {
+  return generateApiQuery({
+    variables: { id, title, startDate, startTime, endDate, endTime, description },
+    query: UPDATE_ACTIVITY_MUTATION,
+  });
+}
+
+export function deleteArticle(id: string): Promise<any> {
+  return generateApiQuery({
+    variables: { id },
+    query: DELETE_ARTICLE_MUTATION,
+  });
+}
