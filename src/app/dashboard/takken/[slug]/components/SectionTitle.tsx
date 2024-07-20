363c72ecb2d2d8c27e16ee5fdb13dad7c0030b6e
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-07-20 15:59:08 +0200

    Fix modal functionality

--- src/app/dashboard/takken/[slug]/components/SectionTitle.tsx
+++ src/app/dashboard/takken/[slug]/components/SectionTitle.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import { useState, useRef, useEffect, useCallback } from 'react';
 import Modal from '@/components/atoms/Modal';
 import ActionTitle from '@/components/molecules/ActionTitle';
 import Form from '@/components/organisms/Forms';
-import { useRef } from 'react';
 
 type SectionTitleProps = {
   title: string;
@@ -12,19 +12,77 @@ type SectionTitleProps = {
 };
 
 const SectionTitle = ({ title, groupId, type }: SectionTitleProps) => {
-  const modalRef = useRef<HTMLDialogElement>(null);
+  const modal = useRef<HTMLDialogElement>(null);
+  const [toggle, setToggle] = useState<boolean>(false);
 
-  const handleOpenModal = () => {
-    if (modalRef.current) {
-      modalRef.current.showModal();
-    }
-  };
+  const openDropdown = useCallback(() => {
+    modal.current?.showModal();
+    document.body.setAttribute('style', 'overflow-y: hidden');
+  }, [modal]);
 
-  const handleCloseModal = () => {
-    if (modalRef.current) {
-      modalRef.current.close();
+  const closeDropdown = useCallback(() => {
+    setToggle(false);
+    modal.current?.close();
+    document.body.removeAttribute('style');
+  }, [modal]);
+
+  useEffect(() => {
+    const closeKeyDownHandler = (event: KeyboardEvent) => {
+      if (event.key !== 'Escape') return;
+      closeDropdown();
+      document.removeEventListener('keydown', closeKeyDownHandler);
+    };
+
+    const clickHandler = (event: MouseEvent) => {
+      const target = event.target as Node;
+
+      if (!modal || !modal.current || !target) {
+        return;
+      }
+
+      const inner = document.querySelector('#modal__inner');
+      if (!inner || !(inner instanceof HTMLElement)) {
+        return;
+      }
+
+      if (inner.contains(target)) {
+        return;
+      }
+
+      const rect = inner.getBoundingClientRect();
+      if (
+        event.clientY >= rect.top &&
+        event.clientY <= rect.bottom &&
+        event.clientX >= rect.left &&
+        event.clientX <= rect.right
+      ) {
+        return;
+      }
+
+      closeDropdown();
+      document.removeEventListener('click', clickHandler);
+    };
+
+    if (toggle) {
+      document.addEventListener('keydown', closeKeyDownHandler);
+      document.addEventListener('click', clickHandler);
     }
-  };
+
+    return () => {
+      document.removeEventListener('keydown', closeKeyDownHandler);
+      document.removeEventListener('click', clickHandler);
+    };
+  });
+
+  const openClickHandler = useCallback(() => {
+    setToggle(!toggle);
+    openDropdown();
+  }, [toggle, openDropdown]);
+
+  const closeClickHandler = useCallback(() => {
+    setToggle(false);
+    closeDropdown();
+  }, [closeDropdown]);
 
   return (
     <>
@@ -33,13 +91,13 @@ const SectionTitle = ({ title, groupId, type }: SectionTitleProps) => {
         tagName="h2"
         button={{
           label: 'Nieuwe activiteit',
-          onClick: handleOpenModal,
+          onClick: openClickHandler,
         }}
       />
-      <Modal ref={modalRef} closeModal={handleCloseModal} title="Nieuwe activiteit">
+      <Modal ref={modal} closeModal={closeClickHandler} title="Nieuwe activiteit">
         <Form
           variant="activity"
-          props={{ groupId, handleCloseModal }}
+          props={{ groupId, closeClickHandler }}
           blockProperties={{ slug: 'activity-new', modNoPadding: true }}
         />
       </Modal>
--- src/components/atoms/Modal/index.tsx
+++ src/components/atoms/Modal/index.tsx
@@ -1,8 +1,8 @@
 import { forwardRef } from 'react';
+import { IconClose } from '@/assets/icons';
+import Icon from '@/components/atoms/Icon';
 import { Modal as ModalProps } from './types';
 import styles from './Modal.css';
-import Icon from '@/components/atoms/Icon';
-import { IconClose } from '@/assets/icons';
 
 export const links = () => {
   return [{ rel: 'stylesheet', href: styles }];
@@ -14,18 +14,20 @@ const Modal = forwardRef<HTMLDialogElement, Props>(
   ({ title, closeModal, children }: Props, ref) => {
     return (
       <dialog className="modal" ref={ref} role="none">
-        <div className="modal__header">
-          <button
-            className="modal__header__close-button"
-            type="button"
-            aria-label="Venster sluiten"
-            onClick={closeModal}
-          >
-            <Icon icon={IconClose} title="Close" size={'lg'} />
-          </button>
-          <div className="t-headline-3">{title}</div>
+        <div id="modal__inner">
+          <div className="modal__header">
+            <button
+              className="modal__header__close-button"
+              type="button"
+              aria-label="Venster sluiten"
+              onClick={closeModal}
+            >
+              <Icon icon={IconClose} title="Close" size={'lg'} />
+            </button>
+            <div className="t-headline-3">{title}</div>
+          </div>
+          <div className="modal__content">{children}</div>
         </div>
-        <div className="modal__content">{children}</div>
       </dialog>
     );
   },
