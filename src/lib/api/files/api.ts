80b53315a6ffe8ad67bd570caba08b7324a24ddf
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-07-22 21:18:28 +0200

    Add delete file mutation

---
+++ src/lib/api/files/api.ts
@@ -0,0 +1,9 @@
+import { generateApiQuery } from '@/lib/api';
+import { DELETE_FILE_MUTATION } from './mutations';
+
+export function deleteFile(id: string): Promise<any> {
+  return generateApiQuery({
+    variables: { id },
+    query: DELETE_FILE_MUTATION,
+  });
+}
