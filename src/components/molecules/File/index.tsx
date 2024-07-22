b0c888d7ce02b7723c4881636c18f95a7ab9642e
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-07-22 21:19:44 +0200

    Upgrade file component

--- src/components/molecules/File/index.tsx
+++ src/components/molecules/File/index.tsx
@@ -1,6 +1,15 @@
 'use client';
 
-import { IconTextFile, IconPresentationFile, IconImageFile, IconFile } from '@/assets/icons';
+import { useState } from 'react';
+import { deleteFile } from '@/lib/api/files/api';
+import {
+  IconTextFile,
+  IconPresentationFile,
+  IconImageFile,
+  IconFile,
+  IconClose,
+} from '@/assets/icons';
+import Loader from '@/components/atoms/Loader';
 import Icon from '@/components/atoms/Icon';
 import Typography from '@/components/atoms/Typography';
 import { File as FileBlockProps, extensions } from './types';
@@ -21,9 +30,12 @@ const extMap: extensions = {
   jpg: IconImageFile,
   jpeg: IconImageFile,
   png: IconImageFile,
+  gif: IconImageFile,
 };
 
-const File = ({ ext, url, name, size }: Props) => {
+const File = ({ id, ext, url, name, size, modDeleteable, deleteCallback }: Props) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
   const download = () => {
     const a = document.createElement('a');
     a.href = url;
@@ -33,13 +45,42 @@ const File = ({ ext, url, name, size }: Props) => {
     document.body.removeChild(a);
   };
 
+  const handleDeleteFile = async (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setLoading(true);
+
+    if (confirm(`Weet je zeker dat je het bestand "${name}" wil verwijderen?`)) {
+      try {
+        await deleteFile(id);
+        deleteCallback();
+      } catch (err: any) {
+        console.error(err);
+      }
+    }
+
+    setLoading(false);
+  };
+
   const icon = extMap[ext.slice(1)] ? extMap[ext.slice(1)] : IconFile;
 
   return (
-    <div className="file__container" onClick={download}>
-      <Icon size="xl" icon={icon} title="Chevron" />
-      <Typography className="file__name">{name.replaceAll(ext, '')}</Typography>
-      <Typography className="file__filesize">{size}KB</Typography>
+    <div className="file" onClick={download}>
+      <Icon size="xl" icon={icon} title={ext.slice(1)} className="file__icon" />
+      <div className="file__info">
+        <Typography className="file__info__name">{name.replaceAll(ext, '')}</Typography>
+        <Typography className="file__info__filesize">{size}KB</Typography>
+      </div>
+      {modDeleteable && !loading && (
+        <button
+          className="file__delete-button"
+          aria-label="Bestand verwijderen"
+          type="button"
+          onClick={handleDeleteFile}
+        >
+          <Icon size="sm" icon={IconClose} title="Bestand verwijderen" />
+        </button>
+      )}
+      {modDeleteable && loading && <Loader size="xs" />}
     </div>
   );
 };
