b394c9ee492a2fa99587f7b232de392e8ee58db7
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-08-06 23:17:42 +0200

    Remove unused files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,5 @@
-import { ApolloProvider } from '@apollo/client';
-import client from '@/lib/api/apollo/client';
-import { AuthProvider } from '@/lib/api/security/security';
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <ApolloProvider client={client}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
