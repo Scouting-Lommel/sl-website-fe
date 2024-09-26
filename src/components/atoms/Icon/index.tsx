0485898bc1cae3198325d90ca333f3a05f7184c7
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-09-26 08:37:22 +0200

    Import icon styles

--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -2,6 +2,11 @@ import cx from 'classnames';
 import * as icons from 'lucide-react';
 import { iconMap } from './IconMap';
 import { Icon as IconProps } from './types';
+import styles from './Icon.css';
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 const Icon = ({ name, size, className, ...props }: IconProps) => {
   const mappedName = iconMap[name];
