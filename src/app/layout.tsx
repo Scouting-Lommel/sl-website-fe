ddf34b31718f6103b57d4e0c0ee8c9abe84c354c
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-10-08 22:10:23 +0200

    Configure i18n with next-intl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 export const dynamic = 'force-dynamic';
 
+import { NextIntlClientProvider } from 'next-intl';
 import { getServerSession } from 'next-auth';
-import SessionProvider from '@/lib/providers/SessionProvider';
+import { getLocale, getMessages } from 'next-intl/server';
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import { generateMetadataForRootLayout } from '@/lib/helpers/generateMetadata';
+import SessionProvider from '@/lib/providers/SessionProvider';
 import GlobalAlert from '@/components/atoms/GlobalAlert';
-import Header from '@/components/organisms/Header';
 import SkipToContent from '@/components/atoms/SkipToContent';
 import Footer from '@/components/organisms/Footer';
+import Header from '@/components/organisms/Header';
 import { getGeneralData } from './api';
+
 import '@/app/global.css';
 
 export async function generateMetadata() {
@@ -21,41 +24,46 @@ export async function generateMetadata() {
 }
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  const locale = await getLocale();
+  const messages = await getMessages();
+
   const data = await getGeneralData();
   const session = await getServerSession(authOptions);
 
   const globalAlert = data.generalData.data.attributes.globalAlert;
 
   return (
-    <html lang="nl">
+    <html lang={locale}>
       <body>
-        <SessionProvider session={session}>
-          <SkipToContent className="skip-to-content" />
+        <NextIntlClientProvider messages={messages}>
+          <SessionProvider session={session}>
+            <SkipToContent className="skip-to-content" />
 
-          {globalAlert && globalAlert?.enabled && (
-            <GlobalAlert label={globalAlert?.label} variant={globalAlert?.variant} />
-          )}
+            {globalAlert && globalAlert?.enabled && (
+              <GlobalAlert label={globalAlert?.label} variant={globalAlert?.variant} />
+            )}
 
-          <Header
-            logo={data.generalData.data.attributes.logo}
-            mainNavigation={data.generalData.data.attributes.mainNavigation}
-            groups={data.groups.data.map((item: any) => item.attributes)}
-            rentalLocations={data.rentalLocations.data.map((item: any) => item.attributes)}
-          />
+            <Header
+              logo={data.generalData.data.attributes.logo}
+              mainNavigation={data.generalData.data.attributes.mainNavigation}
+              groups={data.groups.data.map((item: any) => item.attributes)}
+              rentalLocations={data.rentalLocations.data.map((item: any) => item.attributes)}
+            />
 
-          <main className="sl-main" id="main">
-            {children}
-          </main>
+            <main className="sl-main" id="main">
+              {children}
+            </main>
 
-          <Footer
-            siteName={data.generalData.data.attributes.siteName}
-            vatNumber={data.generalData.data.attributes.vatNumber}
-            groupNumber={data.generalData.data.attributes.groupNumber}
-            address={data.generalData.data.attributes.address}
-            contactItems={data.generalData.data.attributes.contactItems}
-            footerNavigation={data.generalData.data.attributes.footerNavigation}
-          />
-        </SessionProvider>
+            <Footer
+              siteName={data.generalData.data.attributes.siteName}
+              vatNumber={data.generalData.data.attributes.vatNumber}
+              groupNumber={data.generalData.data.attributes.groupNumber}
+              address={data.generalData.data.attributes.address}
+              contactItems={data.generalData.data.attributes.contactItems}
+              footerNavigation={data.generalData.data.attributes.footerNavigation}
+            />
+          </SessionProvider>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
