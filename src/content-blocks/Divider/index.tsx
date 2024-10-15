cf065ef858cb977875bf5c34140fff088e95f782
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-10-15 18:58:39 +0200

    Add variant prop to divider

--- src/content-blocks/Divider/index.tsx
+++ src/content-blocks/Divider/index.tsx
@@ -3,8 +3,8 @@ import { Divider as DividerProps } from './types';
 
 type Props = DividerProps & React.HTMLAttributes<HTMLElement>;
 
-const DividerBlock = ({}: Props) => {
-  return <Divider />;
+const DividerBlock = ({ variant }: Props) => {
+  return <Divider variant={variant} />;
 };
 
 export default DividerBlock;
