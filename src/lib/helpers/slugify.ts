3128b5dba9f6eb6f380451765e0226df05b3457c
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-11-08 23:03:55 +0100

    Add slugify helper

---
+++ src/lib/helpers/slugify.ts
@@ -0,0 +1,25 @@
+/**
+ * Converts a given string into a URL-friendly slug.
+ *
+ * The function performs the following transformations:
+ * - Normalizes unicode characters using NFKD normalization.
+ * - Converts the string to lowercase.
+ * - Trims leading and trailing whitespace.
+ * - Replaces spaces with hyphens (`-`).
+ * - Removes all non-word characters except hyphens.
+ * - Replaces multiple consecutive hyphens with a single hyphen.
+ * - Removes leading and trailing hyphens.
+ *
+ * @param str - The input string to be slugified.
+ * @returns The slugified version of the input string.
+ */
+export const slugify = (str: string) => {
+  return str
+    .normalize('NFKD')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/^-+|-+$/g, '');
+};
