efd52b630f83f4a8790f42f18e3e95b10bce13f7
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-11-22 14:46:43 +0100

    Add storybook theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,18 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  // Theme base
+  base: 'light',
+
+  // Branding
+  brandTitle: 'Scouting Lommel docs',
+  brandUrl: 'https://scoutinglommel.be',
+  brandImage: 'https://res.cloudinary.com/scoutinglommel/small_logo_naked_66f0c8b726.png',
+  brandTarget: '_self',
+
+  // Colors
+  colorPrimary: '#4d6e5a',
+  colorSecondary: '#edb942',
+  textColor: '#1f2c24',
+  textInverseColor: '#5f7d6b',
+});
