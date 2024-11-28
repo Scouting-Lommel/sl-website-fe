f45e25a335fadf1b95ee37e308d59344603f1218
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-11-28 20:32:12 +0100

    Add Input story

---
+++ src/components/atoms/Forms/Input/Input.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Input from '.';
+
+const meta: Meta<typeof Input> = {
+  title: '3 Components/Atoms/Forms/Input',
+  component: Input,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    id: 'input',
+    name: 'input',
+    label: 'Voornaam',
+    placeholder: 'Voer uw voornaam in',
+  },
+};
+
+export const HasError: Story = {
+  args: {
+    id: 'input',
+    name: 'input',
+    label: 'Voornaam',
+    placeholder: 'Voer uw voornaam in',
+    error: 'Dit veld is verplicht.',
+  },
+};
