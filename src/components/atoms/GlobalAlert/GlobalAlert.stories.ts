148f13627ca50b14c101d0d9187d967531fbb009
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-11-29 10:20:29 +0100

    Add Global Alert story

---
+++ src/components/atoms/GlobalAlert/GlobalAlert.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import GlobalAlert from '.';
+
+const meta: Meta<typeof GlobalAlert> = {
+  title: '3 Components/Atoms/Global Alert',
+  component: GlobalAlert,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: { type: 'select' },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'info',
+    label: 'De status van je inschrijving is gewijzigd.',
+  },
+};
