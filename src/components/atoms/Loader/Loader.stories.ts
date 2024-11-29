966a5b9fb2ac0012850be660f50f7a7d42ed0b22
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-11-29 10:26:30 +0100

    Tweak Loader story

--- src/components/atoms/Loader/Loader.stories.ts
+++ src/components/atoms/Loader/Loader.stories.ts
@@ -5,11 +5,19 @@ const meta: Meta<typeof Loader> = {
   title: '3 Components/Atoms/Loader',
   component: Loader,
   tags: ['autodocs'],
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    size: 'md',
+    modLabelVisible: false,
+  },
 };
