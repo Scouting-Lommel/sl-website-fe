d17f0fc8dcf5f09c0ebae0abbd5cd7fc75d0d870
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-12-03 18:38:37 +0100

    Tweak CarouselItem story

--- src/components/molecules/CarouselItem/CarouselItem.stories.tsx
+++ src/components/molecules/CarouselItem/CarouselItem.stories.tsx
@@ -3,22 +3,8 @@ import { Default as imagedefault } from '@/components/atoms/Image/Image.stories'
 import CarouselItem from '.';
 
 const meta: Meta<typeof CarouselItem> = {
-  title: '3 Components/Molecules/CarouselItem',
+  title: '3 Components/Molecules/Carousel Item',
   component: CarouselItem,
-  argTypes: {
-    name: {
-      control: { type: 'text' },
-      description: 'The name on the carouesl item',
-    },
-    slug: {
-      control: { type: 'text' },
-      description: 'The slug of the item',
-    },
-    logo: {
-      control: { type: 'object' },
-      description: 'The logo of the carousel item',
-    },
-  },
 };
 
 export default meta;
@@ -26,12 +12,19 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    name: 'Test name',
-    slug: 'Test slug',
+    name: 'De leidingsgroep',
+    slug: 'de-leidingsgroep',
     logo: {
       data: {
         attributes: imagedefault.args!.data!,
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <div style={{ width: '300px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
