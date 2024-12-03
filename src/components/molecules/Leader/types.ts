de4816069552fae04a6e32296668c084fd65ab03
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-12-03 19:04:13 +0100

    Remove unused Leader prop

--- src/components/molecules/Leader/types.ts
+++ src/components/molecules/Leader/types.ts
@@ -1,7 +1,6 @@
 import { CloudinaryImage } from '@/components/atoms/Image/types';
 
 export type Leader = {
-  active: boolean;
   firstName: string;
   lastName: string;
   image: { data: { attributes: CloudinaryImage } };
