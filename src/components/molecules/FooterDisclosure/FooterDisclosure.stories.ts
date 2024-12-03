b2275a0c4e6bf77ab0269bd44279a6553612133d
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-12-03 18:56:06 +0100

    Tweak FooterDisclosure story

--- src/components/molecules/FooterDisclosure/FooterDisclosure.stories.ts
+++ src/components/molecules/FooterDisclosure/FooterDisclosure.stories.ts
@@ -2,14 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 import FooterDisclosure from '.';
 
 const meta: Meta<typeof FooterDisclosure> = {
-  title: '3 Components/Molecules/FooterDisclosure',
+  title: '3 Components/Molecules/Footer Disclosure',
   component: FooterDisclosure,
-  argTypes: {
-    siteName: {
-      control: { type: 'text' },
-      description: 'The name of the site',
-    },
-  },
 };
 
 export default meta;
