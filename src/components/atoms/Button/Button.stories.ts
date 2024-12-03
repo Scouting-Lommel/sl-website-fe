974a51b6d8479dc66da037c5b656a762a03a8a60
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-12-03 18:51:09 +0100

    Tweak button story

--- src/components/atoms/Button/Button.stories.ts
+++ src/components/atoms/Button/Button.stories.ts
@@ -23,7 +23,7 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     variant: 'primary',
-    label: "I'm a button!",
+    label: 'More information',
     loading: false,
     modSmall: false,
   },
@@ -32,7 +32,7 @@ export const Default: Story = {
 export const LinkButton: Story = {
   args: {
     variant: 'primary',
-    label: "I'm a button!",
+    label: 'More information',
     href: '#',
     loading: false,
     modSmall: false,
