fb0feed0e393b397394ad47072adfe41f0b442e5
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-12-05 19:30:54 +0100

    Tweak Gallery story

--- src/components/organisms/Gallery/Gallery.stories.ts
+++ src/components/organisms/Gallery/Gallery.stories.ts
@@ -5,20 +5,6 @@ import Gallery from '.';
 const meta: Meta<typeof Gallery> = {
   title: '3 Components/Organisms/Gallery',
   component: Gallery,
-  argTypes: {
-    title: {
-      control: { type: 'text' },
-      description: 'The title',
-    },
-    initialItems: {
-      control: { type: 'number' },
-      description: 'The number of initial items',
-    },
-    images: {
-      control: { type: 'text' },
-      description: 'The images',
-    },
-  },
 };
 
 export default meta;
@@ -26,7 +12,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    title: 'Example title',
+    title: 'Fotogalerij',
     initialItems: 3,
     images: {
       data: [
