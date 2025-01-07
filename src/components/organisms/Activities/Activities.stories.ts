dcf58e5210dce7359138379fbc1bac69afdf687a
Seppe Alaerts <seppe.alaerts@craftzing.com>
2025-01-07 14:07:57 +0100

    Fix activities story

--- src/components/organisms/Activities/Activities.stories.ts
+++ src/components/organisms/Activities/Activities.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { formatDate } from '@/lib/helpers/dateTime';
 import Activities from '.';
 
 const meta: Meta<typeof Activities> = {
@@ -13,29 +12,6 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     initialItems: 3,
-    activities: [
-      {
-        attributes: {
-          title: 'Stratego',
-          startDate: formatDate(new Date()),
-          endDate: formatDate(new Date()),
-          startTime: '12:00',
-          endTime: '14:30',
-          description:
-            'Vandaag spelen we het oerspel der scouts: Stratego! Wie kan zich kronen tot de beste generaal van Scouting Lommel? Kom het te weten op deze knotsgekke namiddag!',
-        },
-      },
-      {
-        attributes: {
-          title: 'Schattentocht',
-          startDate: formatDate(new Date(new Date().getTime() + 24 * 60 * 60 * 1000 * 2 * 7)),
-          endDate: formatDate(new Date(new Date().getTime() + 24 * 60 * 60 * 1000 * 2 * 7)),
-          startTime: '18:30',
-          endTime: '21:00',
-          description:
-            'Vandaag gaan we met z’n allen op zoek naar een grote schat vol met goud die ergens in de Sahara verstopt ligt. De welpenleiding is al weken aan het zoeken naar deze schat maar vindt hem maar niet. Komen jullie ons vandaag hierbij helpen?',
-        },
-      },
-    ],
+    groupSlug: 'akabe',
   },
 };
