36453814e7c52eb62d7fe9f34f3c40540fab1249
Seppe Alaerts <seppe.alaerts@craftzing.com>
2025-07-15 09:46:58 +0200

    Update playground page

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 import { generateMetadataForPage } from '@/lib/helpers/generateMetadata';
 import BlockContainer from '@/components/atoms/BlockContainer';
+import Typography from '@/components/atoms/Typography';
 import Form from '@/components/organisms/Forms';
 import Hero from '@/components/organisms/Hero';
 import { getGeneralData } from '../api';
@@ -24,15 +25,25 @@ export const generateMetadata = async (): Promise<Metadata> => {
 
 const PlaygroundPage = async (): Promise<JSX.Element> => {
   const t = await getTranslations('forms.eetfestijnForm');
+
   return (
     <>
-      <BlockContainer variant="light" orientation="default" slug="eetfestijn-hero" modSmallPadding>
+      <BlockContainer variant="light" orientation="default" slug="hero" modSmallPadding>
         <Hero
-          title={t('hero.title')}
-          subtitle={t('hero.subtitle')}
+          title="Playground"
+          subtitle="Development playground"
           variant="simple"
           className="sl-layout"
         />
+      </BlockContainer>
+
+      <hr />
+
+      <BlockContainer variant="light" orientation="default" slug="eetfestijn-hero" modSmallPadding>
+        <div className="sl-layout">
+          <h2>{t('hero.title')}</h2>
+          <Typography>{t('hero.subtitle')}</Typography>
+        </div>
       </BlockContainer>
 
       <Form variant="eetfestijn" blockProperties={{ slug: 'eetfestijn-form' }} />
