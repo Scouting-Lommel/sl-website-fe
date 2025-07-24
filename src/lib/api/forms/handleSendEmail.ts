4eaaf1362b46ec2e51ed06fdbf0c8a43dd2da321
Seppe Alaerts <seppe.alaerts@craftzing.com>
2025-07-24 08:40:07 +0200

    Throw error when no mailgun api key is not set

--- src/lib/api/forms/handleSendEmail.ts
+++ src/lib/api/forms/handleSendEmail.ts
@@ -4,12 +4,17 @@ import { NextResponse } from 'next/server';
 import { Email } from '@/lib/helpers/sendEmail';
 
 export const handleSendEmail = async (email: Email): Promise<NextResponse> => {
+  if (!process.env.MAILGUN_API_KEY) {
+    return NextResponse.json({ error: 'MAILGUN_API_KEY is not set' }, { status: 500 });
+  }
+
   const mailgun = new Mailgun(FormDataLib);
   const mg = mailgun.client({
     username: 'api',
-    key: process.env.MAILGUN_API_KEY || 'API_KEY',
+    key: process.env.MAILGUN_API_KEY,
     url: 'https://api.eu.mailgun.net',
   });
+
   try {
     await mg.messages.create('mg.scoutinglommel.be', email as any);
     return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
