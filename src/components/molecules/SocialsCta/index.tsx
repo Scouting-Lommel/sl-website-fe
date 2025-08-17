bb8593fbac913121ecf6a24cb8989d4372f55a2f
Seppe Alaerts <seppe.alaerts@craftzing.com>
2025-08-17 10:04:17 +0200

    Add visually hidden labels to social links

--- src/components/molecules/SocialsCta/index.tsx
+++ src/components/molecules/SocialsCta/index.tsx
@@ -1,7 +1,7 @@
 import cn from 'classnames';
 import { StylesheetLink } from '@/types/StyleSheetLink';
 import Icon from '@/components/atoms/Icon';
-import { iconMap, IconNames } from '@/components/atoms/Icon/IconMap';
+import { IconNames } from '@/components/atoms/Icon/IconMap';
 import SLLink from '@/components/atoms/Link';
 import { SocialsCta as SocialsCtaProps } from './types';
 import styles from './SocialsCta.css';
@@ -36,11 +36,11 @@ const SocialsCta = ({ title, socialItems, className }: SocialsCtaProps): JSX.Ele
             >
               <Icon
                 name={icons[item.icon]}
-                aria-label={item.title}
                 size="lg"
                 strokeWidth={2.5}
                 className="socials-cta__socials__item__icon"
               />
+              <span className="u-visually-hidden">{item.title}</span>
             </SLLink>
           );
         })}
